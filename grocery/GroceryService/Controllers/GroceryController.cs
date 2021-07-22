131db8458f1f9f15442e3be167266101bfb03575
Ranga Senal Fernando <ranga.fernando@gmail.com>
2021-07-22 23:24:20 +1000

    Add a new controller

---
+++ grocery/GroceryService/Controllers/GroceryController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GroceryController : ControllerBase
+    {
+        public GroceryController()
+        {
+            
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok("Hello this is Grocery controller");
+
+        }
+    }
+    
+}
