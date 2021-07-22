7c0a455d85de6d871117fa9454d65a34d0077c63
Ranga Senal Fernando <ranga.fernando@gmail.com>
2021-07-22 23:06:50 +1000

    Dockerized Lottery service

---
+++ lottery/LotteryService/Controllers/LotteryController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LotteryService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LotteryController : ControllerBase 
+    {
+        public LotteryController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Lottery service");
+        }
+    }
+}
