805449847ecd5a0189f56d5d1f360e1ebf923bb8
senal <ranga.fernando@gmail.com>
2021-08-06 21:54:57 +1000

    Add controller to paper service api

---
+++ papers/PaperService/Controllers/PaperController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace PaperService.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PaperController : ControllerBase
+    {
+        public PaperController()
+        {
+            
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+             return Ok("This is the Paper controller");   
+
+        }        
+    }
+    
+}
